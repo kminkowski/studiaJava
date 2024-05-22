724cded436a85255b9423d8af6b480fb625337e0
kminkowski <minkowskikamil@gmail.com>
2024-05-22 14:51:38 +0200

    ok

---
+++ src/main/java/org/example/CarRental/Controllers/MainWebLayout.java
@@ -0,0 +1,21 @@
+//package org.example.CarRental.Controllers;
+//
+//import org.example.CarRental.HtmlSchema;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class MainWebLayout{
+//
+//    @Autowired
+//    private HtmlSchema htmlSchema;
+//
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String getHtmlSchema() {
+//        return HtmlSchema.htmlSchema();
+//    }
+//}
+//
